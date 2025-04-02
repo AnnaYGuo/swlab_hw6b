a018be8a0c3b0d75537996df29ee9c98256dfd5d
AnnaYGuo <Annayuab@icloud.com>
2025-04-02 07:57:39 -0500

    build path static

--- backend/hw6.py
+++ backend/hw6.py
@@ -3,7 +3,7 @@ import os
 # from flask_cors import CORS
 
 # app = Flask(__name__)
-app = Flask(__name__, static_folder='../frontend/build/static', static_url_path='/')
+app = Flask(__name__, static_folder='../frontend/build/', static_url_path='/')
 # CORS(app) # for locally running this
 
 @app.route("/")
